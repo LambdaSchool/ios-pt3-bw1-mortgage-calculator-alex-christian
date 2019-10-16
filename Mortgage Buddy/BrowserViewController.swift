e3cf5f3591ccbdcb4cdcefce55e6de78832ea4ab
Christian Lorenzo <clbmiami2004@gmail.com>
2019-10-15 23:07:33 -0700

    Created a CocoaTouch document named BrowserViewController along with a new View Controller, which are intended to manage the app browser.

---
+++ Mortgage Buddy/BrowserViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BrowserViewController.swift
+//  Mortgage Buddy
+//
+//  Created by Lambda_School_Loaner_201 on 10/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class BrowserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
