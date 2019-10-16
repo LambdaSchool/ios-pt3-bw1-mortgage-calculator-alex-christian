83b67dde2e133a72ee748bdcd55a30045ef69296
Christian Lorenzo <clbmiami2004@gmail.com>
2019-10-16 00:00:52 -0700

    Added code in order to make the browser run with some relevant pages and a Google browser.

--- Mortgage Buddy/BrowserViewController.swift
+++ Mortgage Buddy/BrowserViewController.swift
@@ -9,23 +9,46 @@
 import UIKit
 import WebKit
 
-class BrowserViewController: UIViewController {
+class BrowserViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "WebSites", style: .plain, target: self, action: #selector(openTapped))
 
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.google.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open Page", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Trulia.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Zillow.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Realtor.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
     }
-    */
+    
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else {return}
+        guard let url = URL(string: "https://" + actionTitle) else {return}
+        webView.load(URLRequest(url: url))
+        
+    }
+   
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 
 }
