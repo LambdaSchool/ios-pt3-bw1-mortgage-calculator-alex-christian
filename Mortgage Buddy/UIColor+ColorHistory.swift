3cc2c8b870d1d4c6c41d6f577fcb12c0305de220
Christian Lorenzo <clbmiami2004@gmail.com>
2019-10-19 21:00:05 -0700

    Christian PR
    Edited Tips viewController fixed some errors.

---
+++ Mortgage Buddy/UIColor+ColorHistory.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+extension UIColor{
+    
+
+    static let charlotte = UIColor(red: 0.63, green: 0.86, blue: 0.91, alpha: 1.00)
+	
+
+	static let darkBlue = UIColor(red: 0.04, green: 0.38, blue: 0.58, alpha: 1.00)
+	
+
+	static let lightBlue = UIColor(red: 0.08, green: 0.82, blue: 0.93, alpha: 1.00)
+	
+    static let darkRed = UIColor(red: 0.55, green: 0.30, blue: 0.40, alpha: 1.00)
+
+
+}
