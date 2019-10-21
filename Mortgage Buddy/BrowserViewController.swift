03ad35e8bafb738c3cef12906e1cb4b6a8e0384d
Alex Thompson <guitarman226@gmail.com>
2019-10-20 19:03:05 -0500

    edited TipsViewsController, added a webKit view in the browserViewController, added constraints, fixed the browser taking up too much of the screen.

--- Mortgage Buddy/BrowserViewController.swift
+++ Mortgage Buddy/BrowserViewController.swift
@@ -11,30 +11,28 @@ import WebKit
 
 class BrowserViewController: UIViewController, WKNavigationDelegate {
     
-    var webView: WKWebView!
+    @IBOutlet weak var webKitView: WKWebView!
     
-    override func loadView() {
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Home Search", style: .plain, target: self, action: #selector(openTapped))
+        webKitView.navigationDelegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Home Search Options", style: .plain, target: self, action: #selector(openTapped))
 
-        let url = URL(string: "https://www.google.com")!
-        webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
+        let url = URL(string:"https://Realtor.com")!
+        webKitView.load(URLRequest(url: url))
+        webKitView.allowsBackForwardNavigationGestures = true
     }
     
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open Page", message: nil, preferredStyle: .actionSheet)
         ac.addAction(UIAlertAction(title: "Google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Realtor.com", style: .default, handler: openPage))
         ac.addAction(UIAlertAction(title: "Trulia.com", style: .default, handler: openPage))
         ac.addAction(UIAlertAction(title: "Zillow.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "Realtor.com", style: .default, handler: openPage))
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -43,7 +41,7 @@ class BrowserViewController: UIViewController, WKNavigationDelegate {
     func openPage(action: UIAlertAction) {
         guard let actionTitle = action.title else {return}
         guard let url = URL(string: "https://" + actionTitle) else {return}
-        webView.load(URLRequest(url: url))
+        webKitView.load(URLRequest(url: url))
         
     }
    
