033ab7556ae0465c862fffe3c58f69aeeca423f9
Alex Thompson <guitarman226@gmail.com>
2019-10-22 17:01:49 -0500

    Made changes in the Home tab and calculator tab
    Made a basic home page, and made changes to the mortgage calculator navigation controller

---
+++ Mortgage Buddy/HomeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeViewController.swift
+//  Mortgage Buddy
+//
+//  Created by Alex Thompson on 10/22/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.layer.cornerRadius = 12
+        imageView.layer.cornerCurve = .continuous
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
