597949edb9df298435fe5995f84ecd2937a89dc2
Christian Lorenzo <clbmiami2004@gmail.com>
2019-10-23 20:25:10 -0700

    Configured Quote Calculator. Working 100%

--- Mortgage Buddy/PriceViewController.swift
+++ Mortgage Buddy/PriceViewController.swift
@@ -17,10 +17,20 @@ class PriceViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var totalIncomeTextView: UILabel!
     
+    
     @IBOutlet weak var affordablePrice: UILabel!
     
+    
     @IBAction func totalIncomeTimes3(_ sender: UIButton) {
-    }
+        print("You just tapped me")
+        guard let yourIncomeString = yourIncomeTextField.text, !yourIncomeString.isEmpty, let yourIncome = Double(yourIncomeString) else {return}
+        guard let spouseIncomeString = spouseIncomeTextField.text, !spouseIncomeString.isEmpty, let spouseIncome = Double(spouseIncomeString) else {return}
+        //guard let totalIncome = totalIncomeTextView.text else {return false}
+        var startingPrice = 0
+        var newTotal = yourIncome + spouseIncome
+        var homeTotal = newTotal * 3
+        affordablePrice.text = "You can afford a house for around $\(homeTotal)"
+}
     
     
 
@@ -30,20 +40,43 @@ class PriceViewController: UIViewController, UITextFieldDelegate {
         // Do any additional setup after loading the view.
     }
     
-//    func textFieldDidEndEditing(_ textField: UITextField) {
-//        print(textField.text)
-//    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(textField.text)
+    }
 
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        guard let yourIncomeString = yourIncomeTextField.text, !yourIncomeString.isEmpty, let yourIncome = Double(yourIncomeString) else {return false}
-        guard let spouseIncomeString = spouseIncomeTextField.text, !spouseIncomeString.isEmpty, let spouseIncome = Double(spouseIncomeString) else {return false}
+        
+        // Hello, Wor
+        //           ^l
+        // Hello, Worl
+        
+        guard var yourIncomeString = yourIncomeTextField.text else { return false }
+        guard var spouseIncomeString = spouseIncomeTextField.text else { return false }
+        
+        switch textField {
+        case yourIncomeTextField:
+            yourIncomeString = yourIncomeString.replacingCharacters(in: Range(range, in: yourIncomeString)!, with: string)
+        case spouseIncomeTextField:
+            spouseIncomeString = spouseIncomeString.replacingCharacters(in: Range(range, in: spouseIncomeString)!, with: string)
+        default:
+            break
+        }
+        
+        if yourIncomeString.isEmpty || spouseIncomeString.isEmpty {
+            totalIncomeTextView.text = ""
+            return true
+        }
+        
+        guard let yourIncome = Double(yourIncomeString) else {return false}
+        guard let spouseIncome = Double(spouseIncomeString) else {return false}
         //guard let totalIncome = totalIncomeTextView.text else {return false}
         var startingPrice = 0
         var newTotal = yourIncome + spouseIncome
-        
-        
-        
-        
+
+        // set the label
+        totalIncomeTextView.text = "$\(newTotal)"
+
+
         return true
     }
 }
